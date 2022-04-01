b54c0bef761795570fdfaaa100148b605b13ae6c
Dimex19 <azeezabolaji06@gmail.com>
2022-04-01 02:40:46 -0700

    task 1 commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * int argc - counts the number of arguments
+ * int argv - contains the arguments
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%d\n",argc);
+	return (0);
+)
