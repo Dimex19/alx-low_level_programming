78721a50f73f497cffaaab36f732e2b17148a608
Dimex19 <azeezabolaji06@gmail.com>
2022-04-01 03:46:46 -0700

    task 0 commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * argc - counts the number of arguments
+ * argv - contains the arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
  * int argv - contains the arguments
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n",argc);
+	printf("%d\n", argc);
 	return (0);
 )
