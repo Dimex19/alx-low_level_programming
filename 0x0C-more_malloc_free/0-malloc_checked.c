cb2280dccee21d21286fe6c9e42b8c0a444d8c47
Dimex19 <azeezabolaji06@gmail.com>
2022-04-07 09:26:03 -0700

    task 0 commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,3 +1,11 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a function that allocates memory using malloc.
+ * @b: size of memory to allocate
+ * Return: Pointer to the address
+*/
+
 void *malloc_checked(unsigned int b)
 {
 	int *size;
