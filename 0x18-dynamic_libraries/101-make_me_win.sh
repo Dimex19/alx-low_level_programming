b4ade6b240235783c5aaf11dc74642a7ba83d9b8
Dimex19 <72265654+Dimex19@users.noreply.github.com>
2022-06-14 02:54:00 +0100

    Update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Dimex19/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/Dimex19/alxschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
